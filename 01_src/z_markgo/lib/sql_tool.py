3f85040f6c9c6edb9c2d78c5c7ff447f6c4d89f8
37ADFB00447625D1599333442193F078 <37ADFB00447625D1599333442193F078@i-search.com.cn>
2019-08-01 18:05:40 +0800

    bug修改

--- 01_src/z_markgo/lib/sql_tool.py
+++ 01_src/z_markgo/lib/sql_tool.py
@@ -1,5 +1,5 @@
 # 原生sql分页查询
-def mysql_page(db,sql,limit,offset):
+def mysql_page(db,sql,offset,limit):
     total = db.session.execute("select count(*) from (" +sql+") t").fetchall()[0][0]
     sql = sql + " limit %s ,%s "%(offset,limit)
     res = db.session.execute(sql).fetchall()
