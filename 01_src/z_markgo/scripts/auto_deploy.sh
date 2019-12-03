a4d9101c092da4934ab1ef4dc6006402863df9cf
37ADFB00447625D1599333442193F078 <37ADFB00447625D1599333442193F078@i-search.com.cn>
2019-12-03 13:59:01 +0800

    Create auto_deploy.sh

---
+++ 01_src/z_markgo/scripts/auto_deploy.sh
@@ -0,0 +1,10 @@
+set -e
+git pull
+#自动编译
+echo "自动编译"
+./compileall.sh
+echo "删除运行中的容器"
+docker stop z_markgo
+docker rm z_markgo
+echo "启动新容器"
+docker run -d -p 5002:5002 --name z_markgo -e SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:root@192.168.1.150:3306/z_markgo?charset=utf8' -v ~/tmp/z_markgo_items:/opt/z_markgo_items z_markgo:1.0.1
