d5636737b3d5dfcc115c4cd93e4cf00a1d15ef7b
37ADFB00447625D1599333442193F078 <37ADFB00447625D1599333442193F078@i-search.com.cn>
2019-12-04 10:55:13 +0800

    Update base_model.py

--- 01_src/z_markgo/dao/base_model.py
+++ 01_src/z_markgo/dao/base_model.py
@@ -13,7 +13,7 @@ class SysPermissionGroup(Base):
     id = Column(INTEGER(11), primary_key=True)
     group_name = Column(String(255))
     key = Column(String(255))
-    parant_key = Column(String(255))
+    parent_key = Column(String(255))
 
 class SysMenu(Base):
     __tablename__ = 'sys_menu'
