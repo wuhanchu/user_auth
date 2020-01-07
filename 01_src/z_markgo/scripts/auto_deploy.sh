2d78cc866f0190efaa3700184b2150b28dbecf21
chenzichu@dataknown.cn <37ADFB00447625D1599333442193F078@i-search.com.cn>
2020-01-07 17:02:14 +0800

    更新脚本

--- 01_src/z_markgo/scripts/auto_deploy.sh
+++ 01_src/z_markgo/scripts/auto_deploy.sh
@@ -1,10 +1,18 @@
 set -e
+#进入脚本所在目录
+currect_path=`cd $(dirname $0);pwd`
+cd $currect_path
+#设置项目名称
+APP_NAME=z_markgo
+echo "项目名称${APP_NAME}"
+echo "更新git代码"
 git pull
 #自动编译
 echo "自动编译"
-sh ./scripts/compileall.sh
+sh compileall.sh ${APP_NAME}
 echo "删除运行中的容器"
-docker stop z_markgo
-docker rm z_markgo
+docker stop $APP_NAME || echo "未发现旧容器"
+docker rm $APP_NAME || echo "未发现旧容器"
 echo "启动新容器"
-docker run -d -p 5002:5002 --name z_markgo -e SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:root@192.168.1.150:3306/z_markgo?charset=utf8' -v ~/tmp/z_markgo_items:/opt/z_markgo_items z_markgo:1.0.0.0
+docker run -d -p 5002:5002 --name $APP_NAME -e SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:root@192.168.1.150:3306/$APP_NAME?charset=utf8' $APP_NAME:1.0.0.0
+
--- 01_src/z_markgo/scripts/compileall.sh
+++ 01_src/z_markgo/scripts/compileall.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+#设置项目名
+APP_NAME=$1
 set -e
 #复制代码到
-echo "复制代码到../z_markgo_cp"
-rm -rf ../z_markgo_cp
-cp -r ../z_markgo ../z_markgo_cp
-cd ../z_markgo_cp
+echo "复制代码到../${APP_NAME}_cp"
+rm -rf ../../${APP_NAME}_cp
+cp -r ../../${APP_NAME} ../../${APP_NAME}_cp
+cd ../../${APP_NAME}_cp
 echo "复制代码成功！"
 echo "开始代码混淆！"
 python36 ./scripts/obscure.py
@@ -12,16 +14,17 @@ echo "代码混淆结束！"
 
 echo "开始代码编译！"
 #选择python编译器版本
-python -m compileall .
-for file in $(find . -name '*.pyc'); 
+python3 -m compileall .
+for file in $(find . -name '*.pyc');
 do
-    mv $file $(echo $file | sed 's/\.cpython-36//g'| sed 's/__pycache__\///g');	
+    mv $file $(echo $file | sed 's/\.cpython-36//g'| sed 's/__pycache__\///g');
 done
 find -name "__pycache__" | awk '{print "rm -rf "$1}'|sh
 find -name "*.py" | awk '{print "rm -rf "$1}'|sh
 echo "代码编译完成！"
 echo "开始生成docker 镜像！"
-docker build . -t z_markgo:1.0.1
-echo "生成docker 镜像：z_markgo:1.0.1！"
-rm -rf ../z_markgo_cp
-echo "删除临时文件z_markgo_cp！"
+docker build . -t ${APP_NAME}:1.0.1
+echo "生成docker 镜像：${APP_NAME}:1.0.1！"
+rm -rf ../${APP_NAME}_cp
+echo "删除临时文件${APP_NAME}_cp！"
+
