7fe0d17afeb5d0e943c3cade9d5c0dedc365e9e0
chenzichu@dataknown.cn <37ADFB00447625D1599333442193F078@i-search.com.cn>
2020-01-07 17:04:31 +0800

    更新脚本

--- 01_src/z_markgo/scripts/auto_deploy.sh
+++ 01_src/z_markgo/scripts/auto_deploy.sh
@@ -4,6 +4,7 @@ currect_path=`cd $(dirname $0);pwd`
 cd $currect_path
 #设置项目名称
 APP_NAME=z_markgo
+VERSION=1.0.0.0
 echo "项目名称${APP_NAME}"
 echo "更新git代码"
 git pull
@@ -14,5 +15,5 @@ echo "删除运行中的容器"
 docker stop $APP_NAME || echo "未发现旧容器"
 docker rm $APP_NAME || echo "未发现旧容器"
 echo "启动新容器"
-docker run -d -p 5002:5002 --name $APP_NAME -e SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:root@192.168.1.150:3306/$APP_NAME?charset=utf8' $APP_NAME:1.0.0.0
+docker run -d -p 5002:5002 --name $APP_NAME -e SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:root@192.168.1.150:3306/$APP_NAME?charset=utf8' $APP_NAME:${VERSION}
 
--- 01_src/z_markgo/scripts/compileall.sh
+++ 01_src/z_markgo/scripts/compileall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #设置项目名
 APP_NAME=$1
+VERSION=$2
 set -e
 #复制代码到
 echo "复制代码到../${APP_NAME}_cp"
@@ -23,8 +24,8 @@ find -name "__pycache__" | awk '{print "rm -rf "$1}'|sh
 find -name "*.py" | awk '{print "rm -rf "$1}'|sh
 echo "代码编译完成！"
 echo "开始生成docker 镜像！"
-docker build . -t ${APP_NAME}:1.0.1
-echo "生成docker 镜像：${APP_NAME}:1.0.1！"
+docker build . -t ${APP_NAME}:${VERSION}
+echo "生成docker 镜像：${APP_NAME}:${VERSION}！"
 rm -rf ../${APP_NAME}_cp
 echo "删除临时文件${APP_NAME}_cp！"
 
