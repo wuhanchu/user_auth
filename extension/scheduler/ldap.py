d39c761b2026aec6f803378683d77fe8b68760bd
wuhanchu <whcwuhanchu@gmail.com>
2020-09-02 10:20:35 +0800

    更新 ldap

--- extension/scheduler/ldap.py
+++ extension/scheduler/ldap.py
@@ -1,62 +1,54 @@
-from ldap3 import Server, Connection, ALL, NTLM
+from ldap3 import Server, Connection, ALL, NTLM, ALL_ATTRIBUTES
 import datetime
 
 
-class operate_AD():
-    def init(self, Domain, User, Password):
-        self.domain = Domain
-        self.user = User
-        self.pwd = Password
-        self.DC = ','.join(['DC=' + dc for dc in Domain.split('.')])
-        self.pre = Domain.split('.')[0].upper()
-        self.server = Server(self.domain, use_ssl=True, get_info=ALL)
-        self.conn = Connection(self.server, user=self.pre + '\\' + self.user, password=self.pwd, auto_bind=True)
-        self.u_time = datetime.date.today()
+class LadpServer():
 
-    def Get_All_UserInfo(self):
-        '''
+    def __init__(self, domain, user, password):
+        self.domain = domain
+        self.user = user
+        self.password = password
+
+        self.DC = ','.join(['DC=' + dc for dc in domain.split('.')])
+        self.pre = domain.split('.')[0].upper()
+        self.server = Server(self.domain, use_ssl=True, get_info=ALL)
+        self.conn = Connection(self.server, user=self.pre + '\\' + self.user, password=self.password, auto_bind=True)
+
+    def get_all_user_info(self):
+        """
         查询组织下的用户
         org: 组织，格式为：aaa.bbb 即bbb组织下的aaa组织，不包含域地址
-        '''
-        att_list = ['displayName', 'userPrincipalName', 'userAccountControl', 'sAMAccountName', 'pwdLastSet']
+        """
+        # att_list = ['displayName', 'userPrincipalName', 'userAccountControl', 'sAMAccountName', 'pwdLastSet']
         # org_base = ','.join(['OU=' + ou for ou in org.split('.')]) + ',' + self.DC
+
         res = self.conn.search(search_base=self.DC, search_filter='(&(objectclass=person)(!(sAMAccountName=*$)))',
-                               attributes=att_list, paged_size='50', search_scope='SUBTREE')
+                               attributes=ALL_ATTRIBUTES, paged_size='50', search_scope='SUBTREE')
         if res:
-            for each in self.conn.response:
-                # print(each['dn'])
-                user = []
-                if len(each) == 5:
-                    user = [each['dn'], each['attributes']['sAMAccountName'], each['attributes']['displayName'],
-                            each['attributes']['userAccountControl'], self.u_time]
-                yield (user)
+            return self.conn.response
         else:
             print('查询失败: ', self.conn.result['description'])
             return None
 
-    def Get_All_GroupInfo(self):
-        '''
-        查询组织下的用户
+    def get_all_group_info(self):
+        """
+        查询组织下的组
         org: 组织，格式为：aaa.bbb 即bbb组织下的aaa组织，不包含域地址
-        '''
-        att_list = ['cn', 'member', 'objectClass', 'userAccountControl', 'sAMAccountName', 'description']
+        """
+        # att_list = ['cn', 'member', 'objectClass', 'userAccountControl', 'sAMAccountName', 'description']
         # org_base = ','.join(['OU=' + ou for ou in org.split('.')]) + ',' + self.DC
-        res = self.conn.search(search_base=self.DC, search_filter='(objectclass=group)', attributes=att_list,
+
+        res = self.conn.search(search_base=self.DC, search_filter='(objectclass=group)', attributes=ALL_ATTRIBUTES,
                                paged_size='',
                                search_scope='SUBTREE')
         if res:
-            for each in self.conn.response:
-                Group = []
-                if len(each) == 5:
-                    for member in each['attributes']['member']:
-                        group = [each['attributes']['sAMAccountName'], member, self.u_time]
-                        yield (group)
+            return self.conn.response
 
         else:
             print('查询失败: ', self.conn.result['description'])
 
 
 if __name__ == '__main__':
-    operation = operate_AD("ad.phfund.com.cn", "x_wuhanchu", "DATAknown1234")
-    operation.Get_All_UserInfo()
-    operation.Get_All_GroupInfo()
+    operation = LadpServer("ad.phfund.com.cn", "x_wuhanchu", "DATAknown1234")
+    print(operation.get_all_user_info())
+    print(operation.get_all_group_info())
