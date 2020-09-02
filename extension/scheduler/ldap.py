5fd41cf6640b4adf8b7e620269007675aa13901a
wuhanchu_dataknown <x_wuhanchu@ad.phfund.com.cn>
2020-09-02 17:24:25 +0800

    添加字段

--- extension/scheduler/ldap.py
+++ extension/scheduler/ldap.py
@@ -3,6 +3,7 @@
 from ldap3 import Server, Connection, ALL, NTLM, ALL_ATTRIBUTES,ALL_OPERATIONAL_ATTRIBUTES,BASE,LEVEL,SUBTREE
 import datetime
 import pysnooper
+import codecs
 
 class LadpServer():
 
@@ -26,7 +27,7 @@ class LadpServer():
         org: 组织，格式为：aaa.bbb 即bbb组织下的aaa组织，不包含域地址
         """
         # att_list = ['displayName',  'member','uid', 'objectClass','userPrincipalName','distinguishedName','objectGUID','objectSid','mail', 'userAccountControl', 'sAMAccountName', 'pwdLastSet','memberof','primaryGroupID']
-        att_list = ['objectGUID','name', 'sAMAccountName','displayName', 'description','mail','memberOf','distinguishedName']
+        att_list = ['objectGUID','name','uid','title','sAMAccountName','mobile','userAccountControl','displayName', 'description','mail','memberOf','distinguishedName']
 
     
         # org_base = ','.join(['OU=' + ou for ou in org.split('.')]) + ',' + self.DC
@@ -35,7 +36,7 @@ class LadpServer():
                                attributes=att_list,search_scope=SUBTREE,generator=True)
 
         if res:
-            return [item.get("attributes") for item in res ] 
+            return [item.get("attributes") for item in res  if item.get("attributes")] 
         else:
             print('查询失败: ', self.conn.result['description'])
             return None
@@ -52,17 +53,17 @@ class LadpServer():
 
         # res = self.conn.search(search_base=self.DC, search_filter='(objectCategory=group)', attributes=att_list)
 
-        res = self.conn.search(search_base=self.DC, search_filter='(&(objectCategory=group))', attributes=att_list)
+        res = self.conn.extend.standard.paged_search(search_base=self.DC, search_filter='(&(objectCategory=group))', attributes=att_list)
         if res:
-            return [item.get("attributes") for item in self.conn.response ] 
-
+            return [item.get("attributes") for item in res if item.get("attributes")] 
         else:
             print('查询失败: ', self.conn.result['description'])
+            return None
 
 
 if __name__ == '__main__':
     operation = LadpServer("ad.phfund.com.cn", "linchengcao", "Qq1612226490@")
-    with open("./temp/user.json","w") as file:
+    with codecs.open("./temp/user.json","w",'utf-8') as file:
         file.write(str(operation.get_all_user_info()))
-    with open("./temp/group.json","w") as file:
+    with codecs.open("./temp/group.json","w",'utf-8') as file:
         file.write(str(operation.get_all_group_info()))
