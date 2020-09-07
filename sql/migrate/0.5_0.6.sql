1728aa433b82463d135ebcdb925f4d959505f7ad
wuhanchu <whcwuhanchu@gmail.com>
2020-09-07 19:14:16 +0800

    update

--- sql/migrate/0.5_0.6.sql
+++ sql/migrate/0.5_0.6.sql
@@ -17,3 +17,4 @@ ALTER TABLE "role_permission_scope"
 ALTER TABLE "user_role"
     ADD CONSTRAINT "user_role_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
+UPDATE "param" SET name = '版本', value = '0.6' WHERE key = 'version';
