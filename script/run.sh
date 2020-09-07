700b0541569b2d2cebd5cffe2455eed0d247f590
wuhanchu <whcwuhanchu@gmail.com>
2020-09-07 09:54:38 +0800

    增加 redis 配置

--- script/run.sh
+++ script/run.sh
@@ -2,6 +2,6 @@ if [ ! -d "./log/" ];then
   mkdir ./log
 fi
 
-gunicorn -w 8 -b 0.0.0.0:5000 run:appl &
+gunicorn -w 8 -b 0.0.0.0:5000 run:app &
 python3 run.py --celery >log/celery_work.log 2>&1  < /dev/null &
 python3 run.py --celery --beat >log/celery_beta.log 2>&1  < /dev/null &
