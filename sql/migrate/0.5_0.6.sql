ad423971b7664d8b00811a95fd0c277d5ccd7c6d
wuhanchu <whcwuhanchu@gmail.com>
2020-09-07 19:54:20 +0800

    修改视图

--- sql/migrate/0.5_0.6.sql
+++ sql/migrate/0.5_0.6.sql
@@ -1,12 +1,10 @@
-ALTER TABLE "role_permission_scope"
-    DROP CONSTRAINT "role_permission_scope_permission_scope_product_key_key_fk";
-
-ALTER TABLE "role_permission_scope"
-    ADD CONSTRAINT "role_permission_scope_permission_scope_product_key_key_fk" FOREIGN KEY ("product_key", "permission_scope_key") REFERENCES "permission_scope" ("key", "product_key") ON DELETE CASCADE ON UPDATE CASCADE;
-
 ALTER TABLE "user_role"
     ADD CONSTRAINT "user_role_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
+create sequence department_id_seq;
+
+ALTER TABLE "department"
+    ALTER COLUMN "id" DROP IDENTITY;
 
 ALTER TABLE "department"
     ALTER COLUMN "id" SET DEFAULT nextval('department_id_seq'::regclass);
@@ -17,4 +15,8 @@ ALTER TABLE "role_permission_scope"
 ALTER TABLE "user_role"
     ADD CONSTRAINT "user_role_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
-UPDATE "param" SET name = '版本', value = '0.6' WHERE key = 'version';
+
+UPDATE "param"
+SET name  = '版本',
+    value = '0.6'
+WHERE key = 'version';
