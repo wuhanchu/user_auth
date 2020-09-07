501ba14d9ca77d7cd97ec26aaf1f69823fec6cc6
unknown <x_wuhanchu@ad.phfund.com.cn>
2020-09-07 18:34:34 +0800

    'update'

--- extension/ldap.py
+++ extension/ldap.py
@@ -40,7 +40,7 @@ class LadpServer():
         查询组织下的组
         org: 组织，格式为：aaa.bbb 即bbb组织下的aaa组织，不包含域地址
         """
-        att_list = ['objectGUID','name','sAMAccountName', 'displayName', 'description','mail','memberOf','distinguishedName','objectClass']
+        att_list = ['objectGUID','name','sAMAccountName','member', 'displayName', 'description','mail','memberOf','distinguishedName','objectClass']
 
         res = self.conn.extend.standard.paged_search(search_base=self.DC, search_filter='(&(objectCategory=group))', attributes=att_list)
 
