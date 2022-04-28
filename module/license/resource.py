f7c49af56f04b7faf267f2d72f9a235dff865e0b
wuhanchu <wuhanchu@dataknown.cn>
2022-04-28 17:00:09 +0800

    用户证书修改

--- module/license/resource.py
+++ module/license/resource.py
@@ -4,7 +4,7 @@ from http import HTTPStatus
 from flask import request, abort
 
 from frame.extension.database import db
-from frame.http.response import JsonResult
+from frame.http.response import Response
 from frame.util.db import auto_commit
 from module.license.util import create_license
 from . import blueprint, util
@@ -12,41 +12,8 @@ from .model import License
 
 from module.auth.extension.oauth2 import require_oauth
 
-@blueprint.route('', methods=['POST'])
-@auto_commit(db.session)
-def license_upload():
-    """
-     证书控制 - 上传证书文件
-    :return:
-    """
-    file = request.files.get("license")
-    content = file.read()
-    file.close()
 
-    is_allow, info = util.check_license(content)
-    if is_allow:
-        record = License(product_key=info["product_key"], content=content.decode())
-        db.session.merge(record)
-
-        return JsonResult.success("注册成功")
-    else:
-        return JsonResult.error("注册失败，%s" % info), HTTPStatus.FORBIDDEN
-
-
-@blueprint.route('/file', methods=['POST'])
-@require_oauth()
-def license_create():
-    """
-    证书创建
-    :return: 证书编码
-    """
-    params = request.json
-
-    return create_license(params.get("machine"), params.get("product_key"), params.get("due_time"),
-                          params.get("custom_name"))
-
-
-@blueprint.route('', methods=['GET'])
+@blueprint.route("", methods=["GET"])
 def license_info():
     """
      查看证书信息
@@ -56,27 +23,22 @@ def license_info():
 
     record = None
     if product_key:
-        record = License.query.get(product_key)
+        record = License.query.filter_by(product_key=product_key).all()
+    else:
+        record = License.query.all()
 
     # 判断证书是否存在，如果存在就返回有效时间
-    if record:
-        is_enable, msg = util.check_license(record.content)
-        if is_enable:
-            return JsonResult.success("证书有效！", dict(msg,
-                                                    machine_info=util.get_machine_info()))
-        else:
-            res = {
-                "machine_info": util.get_machine_info(),
-            }
-            return JsonResult.error("%s ，请联系相关销售进行申请证书！" % msg, res)
-    else:
-        res = {
-            "machine_info": util.get_machine_info()
-        }
-        return JsonResult.error("证书不存在，请联系相关销售进行申请证书！", res)
+    result = {"register_info": util.get_machine_info(), "license": []}
+    
+    for item in record:
+        avaiable, recrod, message = util.check_license(item.content)
+        result["license"].append({**recrod, "avaiable": avaiable, "message": message})
+
+    # 返回
+    return Response(True, data=result).mark_flask_response()
 
 
-@blueprint.route('/check', methods=['get'])
+@blueprint.route("/check", methods=["get"])
 def license_check():
     """
     证书验证
@@ -84,7 +46,63 @@ def license_check():
     """
     product_key = request.args.get("product_key")
     record = License.query.get(product_key)
+    if not record:
+        return Response(
+            False,  message="证书不存在，请联系管理员更新!", http_status=HTTPStatus.UNAUTHORIZED
+        ).mark_flask_response()
+          
+    avaiable, recrod, message = util.check_license(record.content)
+    if not record or not avaiable:
+        return Response(
+            False, data=recrod, message=message, http_status=HTTPStatus.UNAUTHORIZED
+        ).mark_flask_response()
 
-    if not record or not util.check_license(record.content)[0]:
-        abort(HTTPStatus.UNAUTHORIZED, {"message": "证书无效，请联系管理员更新!"})
-    return "证书有效"
+    return Response(
+        data=recrod,
+        message="证书有效!",
+    ).mark_flask_response()
+
+
+@blueprint.route("", methods=["POST"])
+@auto_commit(db.session)
+def license_upload():
+    """
+     证书控制 - 上传证书文件
+    :return:
+    """
+    content = request.json.get("license")
+
+    record_list = content.split("|")
+    for item in record_list:
+        is_allow, info, message = util.check_license(item)
+        if is_allow:
+            record = License(product_key=info["product_key"], content=item)
+            db.session.merge(record)
+        else:
+            db.session.rollback()
+            return Response(False, message="注册失败: %s" % message).mark_flask_response()
+
+    # 成功
+    return Response(message="注册成功").mark_flask_response()
+
+
+@blueprint.route("/file", methods=["POST"])
+def license_create():
+    """
+    证书创建
+    :return: 证书编码
+    """
+    result = []
+    params = request.json
+    product_list = params.get("product")
+    for product in product_list:
+        license = create_license(
+                params.get("register_info"),
+                product.get("product_key"),
+                product.get("due_time"),
+                params.get("custom_name"),
+            )
+        result.append(license)
+
+    # 返回
+    return Response(data={"license":"|".join(result)}).mark_flask_response()
