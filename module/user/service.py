0febb0c027e3d725e59bd9cf4a3c771657f186b6
uglgxu <wuhanchu@me.com>
2022-05-21 22:49:02 +0800

    修改权限校验

--- module/user/service.py
+++ module/user/service.py
@@ -24,7 +24,8 @@ def get_user_extend_info(user_record):
 
 
 def append_permission(data):
-    sql = f"""
+    sql = (
+        f"""
             SET search_path to {db_schema};
             select 
                    p.product_key,
@@ -41,16 +42,47 @@ def append_permission(data):
                      join user_role ur on r.id = ur.role_id
             where ur.user_id = '%s'
             group by p.product_key,p.name, p.url, p.method, p.key;
-        """ % data["id"]
+        """
+        % data["id"]
+    )
 
     res = db.session.execute(sql).fetchall()
     db.session.close()
 
     data["permissions"] = queryToDict(res)
 
+    # 增加不存在的权限
+    no_sql = (
+        f"""
+            SET search_path to {db_schema};
+            select 
+                   permission.product_key,
+                   permission.name,
+                   permission.url,
+                   permission.method,
+                   permission.key
+            from permission where (product_key, key)  not in (
+                select p.product_key,p.key
+                from permission p
+                        join permission_scope_detail rel on rel.permission_key = p.key and rel.product_key = p.product_key
+                        join role_permission_scope gr on gr.permission_scope_key = rel.permission_scope_key and rel.product_key_scope = gr.product_key
+                        join role r on r.id = gr.role_id
+                        join user_role ur on r.id = ur.role_id
+                where ur.user_id = '%s'
+                group by p.product_key,p.name, p.url, p.method, p.key);
+        """
+        % data["id"]
+    )
+
+    res = db.session.execute(no_sql).fetchall()
+    db.session.close()
+
+    data["no_permissions"] = queryToDict(res)
+
 
 def append_permission_scope(data):
-    sql_group = f"""
+    sql_group = (
+        f"""
                 SET search_path to {db_schema};                    
                 select grp.product_key, grp.name, grp.key
                 from permission_scope grp
@@ -58,7 +90,9 @@ def append_permission_scope(data):
                          join role r on r.id = grole.role_id
                          join user_role ur on r.id = ur.role_id
                 where ur.user_id = '%s'
-                """ % data["id"]
+                """
+        % data["id"]
+    )
 
     res_group = db.session.execute(sql_group).fetchall()
     db.session.close()
