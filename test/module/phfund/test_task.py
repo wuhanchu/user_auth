0b8e7a5f059d6295a26c2bee863a0f1e9ced8a41
uglgxu <wuhanchu@me.com>
2022-09-27 10:54:14 +0800

    修改

--- test/module/phfund/test_task.py
+++ test/module/phfund/test_task.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
-import pytest
 import json
-from run import app
-
-from module.phfund.task import sync_data
 
 
 def test_sync_data():
     """
     测试音频转写
     """
+    from module.phfund.task import sync_data
 
     with open("test/data/group.json") as file_obj:
         department_list = json.load(file_obj)
