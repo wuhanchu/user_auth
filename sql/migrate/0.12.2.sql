92c11854e2d462c5edf1e658d4fb2ea7ffadf22b
wuhanchu <wuhanchu@dataknown.cn>
2022-12-01 17:30:46 +0800

    清理无用的授权闲心

---
+++ sql/migrate/0.12.2.sql
@@ -0,0 +1,28 @@
+
+-- 清理无效的权限信息
+delete
+from user_auth.permission_scope
+where product_key is null or product_key not in (SELECT schema_name
+                                                 FROM information_schema.schemata);
+
+delete
+from user_auth.permission_scope_detail
+where product_key is null or product_key not in (SELECT schema_name
+                                                 FROM information_schema.schemata);
+
+delete
+from user_auth.permission
+where product_key is null or product_key not in (SELECT schema_name
+                                                 FROM information_schema.schemata);
+
+delete
+from user_auth.role_permission_scope
+where product_key is null or product_key not in (SELECT schema_name
+                                                 FROM information_schema.schemata);
+
+UPDATE
+    "param"
+SET
+    value = '0.12.2'
+WHERE
+    key = 'version';
