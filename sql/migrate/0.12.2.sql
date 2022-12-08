b9d05f9de3d5503257de547bbe2b39a9c73b4b32
wuhanchu <wuhanchu@dataknown.cn>
2022-12-08 14:09:37 +0800

    修改脚本

--- sql/migrate/0.12.2.sql
+++ sql/migrate/0.12.2.sql
@@ -1,28 +1,30 @@
-
 -- 清理无效的权限信息
-delete
-from user_auth.permission_scope
-where product_key is null or product_key not in (SELECT schema_name
-                                                 FROM information_schema.schemata);
 
 delete
 from user_auth.permission_scope_detail
-where product_key is null or product_key not in (SELECT schema_name
-                                                 FROM information_schema.schemata);
+where product_key is null
+   or product_key not in (SELECT schema_name
+                          FROM information_schema.schemata);
+delete
+from user_auth.permission_scope
+where product_key is null
+   or product_key not in (SELECT schema_name
+                          FROM information_schema.schemata);
+
 
 delete
 from user_auth.permission
-where product_key is null or product_key not in (SELECT schema_name
-                                                 FROM information_schema.schemata);
+where product_key is null
+   or product_key not in (SELECT schema_name
+                          FROM information_schema.schemata);
 
 delete
 from user_auth.role_permission_scope
-where product_key is null or product_key not in (SELECT schema_name
-                                                 FROM information_schema.schemata);
+where product_key is null
+   or product_key not in (SELECT schema_name
+                          FROM information_schema.schemata);
 
 UPDATE
     "param"
-SET
-    value = '0.12.2'
-WHERE
-    key = 'version';
+SET value = '0.12.2'
+WHERE key = 'version';
